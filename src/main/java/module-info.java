a0af0f9942386760ccd9ad1014704c4bcb4a076d
Manuel Osorio <manuelosorio.mkdesignz@gmail.com>
2023-03-20 20:01:04 -0400

    Add maven files from one of example intellij's javafx project
    This also allows me to run the gui in other platforms via maven commands.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,7 @@
-module TextAnalyzerApplication {
+module io.github.manuelosorio {
     requires javafx.controls;
     requires javafx.fxml;
+
+    opens io.github.manuelosorio to javafx.fxml, javafx.graphics, javafx.controls, javafx.base;
+    opens io.github.manuelosorio.controllers to javafx.fxml, javafx.graphics, javafx.controls, javafx.base;
 }
